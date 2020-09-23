76184925092d434756eefbcf884b38c27ace0167
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-09-23 15:11:20 +0300

    Added support for float array and etc...
    Now you can use also a float array with my lib
    And I added precompile commands in case when slp_io.h wasn't include

--- slp_arrays.c
+++ slp_arrays.c
@@ -6,7 +6,13 @@ vsize getArrLen( const char symbol, bool clsMode ) {
 
     while ( !getCorrectInt( &arr_lenght ) || arr_lenght > _MAX_ARR_LEN || arr_lenght < _MIN_ARR_LEN ) {
         if (clsMode) cls;
+
+        #ifdef _SLP_IO
         redPrint( "Размерность не корректна (2-50)\nПожалуйста повторите ввод\n" );
+        #else
+        printf( "Размерность не корректна (2-50)\nПожалуйста повторите ввод\n" );
+        #endif // _SLP_IO
+
         printf( ">> " );
     }
 
@@ -17,7 +23,13 @@ void fillIntArray( iarr arr, const char symbol, vsize len ) {
     for ( vsize i = 0; i < len; i++ ) {
         printf( "%c[%d] >> ", symbol, i + 1 );
         if ( !getCorrectInt( &arr[i] ) ) {
+
+            #ifdef _SLP_IO
             redPrint( "Вы ввели не целое число\n" );
+            #else
+            printf( "Вы ввели не целое число\n" );
+            #endif // _SLP_IO
+
             printf( "Пожалуйста повторите ввод\n" );
             i--;
             continue;
@@ -28,5 +40,29 @@ void fillIntArray( iarr arr, const char symbol, vsize len ) {
 void printIntArray( iarr arr, const char symbol, vsize len, const string status ) {
     greenPrint( "%s массив %c\n", status, symbol );
     for ( vsize i = 0; i < len; i++ ) printf( "%4d  ", arr[i] );
+    nl;
+}
+
+void fillFloatArray( farr arr, const char symbol, vsize len ) {
+    for ( vsize i = 0; i < len; i++ ) {
+        printf( "%c[%d] >> ", symbol, i + 1 );
+        if ( !getCorrectFloat( &arr[i] ) ) {
+
+            #ifdef _SLP_IO
+            redPrint( "Вы ввели не число\nПовторите ввод\n" );
+            #else
+            printf( "Вы ввели не число\nПовторите ввод\n" );
+            #endif // _SLP_IO
+
+            printf( "Пожалуйста повторите ввод\n" );
+            i--;
+            continue;
+        }
+    }
+}
+
+void printFloatArray( farr arr, const char symbol, vsize len, const string status ) {
+    greenPrint( "%s массив %c\n", status, symbol );
+    for ( vsize i = 0; i < len; i++ ) printf( "%7.2f  ", arr[i] );
     nl;
 }
