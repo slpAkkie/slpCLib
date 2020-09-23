d255dc718d8918d00ef6eb3f578d0a6ea48e3b96
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-09-23 15:12:00 +0300

    Added support for float array and etc...
    Now you can use also a float array with my lib
    And I added precompile commands in case when slp_io.h wasn't include

--- slp_arrays.h
+++ slp_arrays.h
@@ -2,18 +2,33 @@
 #include "slp_io.h"
 #include <stdio.h>
 
+// Минимальный размер массива
+#define _MIN_ARR_LEN 2
+// Максимальный размер массива
+#define _MAX_ARR_LEN 50
+
+// Целочисленный массив
 typedef int* iarr;
+
+// Вещественный массив
+typedef float* farr;
 
 #ifndef _VSIZE_T
     #define _VSIZE_T
+    // Размер массива
     typedef unsigned int vsize;
 #endif // !_VSIZE_T
 
+// Установить размер массива
 #define setArrLenght(arr, type, lenght) arr = ( type )malloc( lenght * sizeof( type ) )
 
-#define _MAX_ARR_LEN 50
-#define _MIN_ARR_LEN 2
-
-int getArrLen( const char symbol, bool clsMode );
+// Получает от пользователя размер массива
+vsize getArrLen( const char symbol, bool clsMode );
+// Заполнить целочисленный массив
 void fillIntArray( iarr arr, const char symbol, vsize len );
+// Вывести целочисленный массив
 void printIntArray( iarr arr, const char symbol, vsize len, string status );
+// Заполнить вещественный массив
+void fillFloatArray( farr arr, const char symbol, vsize len );
+// Вывести вещественный массив
+void printFloatArray( farr arr, const char symbol, vsize len, string status );
