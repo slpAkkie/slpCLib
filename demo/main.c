085d5bde6ce8ce73bc151154fd8647b1b177e1c0
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2020-09-23 15:29:59 +0300

    Added coments in Russian

--- demo/main.c
+++ demo/main.c
@@ -5,6 +5,7 @@
 
 
 void main( void ) {
+    // Вызываем инициализацию настроек консоли и передаем название окна
     init( "Тестирование заголовочных файлов" );
 
     while ( true ) {
@@ -13,11 +14,13 @@ void main( void ) {
         if ( !askUserYN( "Хотите пройтись по возможностям библиотеки?" ) ) break;
         cls;
 
+        // Здесь будем хранить ошибки ввода
         bool inputError;
         printf( "Для начала покажу вам защиту на ввод некорректных данных\n" );
 
         greenPrint( "\nСначала защита для ввода целых чисел:\n" );
         printf( "Введите x >> " );
+        // Так можно реализовать обработку неправильного ввода
         int x;
         inputError = getCorrectInt( &x );
         if ( !inputError ) redPrint( "Вы ввели не целое число и сработала защита\n" );
@@ -25,6 +28,7 @@ void main( void ) {
 
         greenPrint( "\nТеперь защита для вещественных чисел:\n" );
         printf( "Введите y >> " );
+        // Теперь обработать ошибку ввода вещественных
         float y;
         inputError = getCorrectFloat( &y );
         if ( !inputError ) redPrint( "Вы ввели не целое число и сработала защита\n" );
@@ -35,8 +39,11 @@ void main( void ) {
 
         bluePrint( "Теперь покажу вам базовую работу с массивами\n\n" );
         char arrAlias = 'X';
+        // Получаем размер массива в переменную типа vsize
         vsize arrLen = getArrLen( arrAlias, false );
+        // Массив создается как обычная переменная типа farr (Если вещественный и iarr если целочисленный)
         farr arrX;
+        // Но ему необходимо задать этим макросом размер
         setArrLenght( arrX, farr, arrLen );
         cls;
 
@@ -50,8 +57,11 @@ void main( void ) {
         cls;
         bluePrint( "Как на счет взглянуть на работу с матрицами?\n" );
         char matrAlias = 'M';
+        // Получаем размер матрицы в переменную типа msize
         msize matr_size = getMatrSize( matrAlias, false );
+        // С матрицей так же, описывается как переменная с указанием типа imatr (Если вещественная и imatr если целочисленная)
         imatr matrX;
+        // Но ей необходимо задать этим макросом размер
         setMatrLenght( matrX, imatr, imatr_el, matr_size );
         fillIntMatr( matr_size, matrX, matrAlias );
         printIntMatr( matr_size, matrX, matrAlias, "Ваша введенная" );
